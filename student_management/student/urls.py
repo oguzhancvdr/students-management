93365cbe649ddaf0725e6c377b3c3152a9997b4e
ogzhncvdr <oguzhancavdar06@gmail.com>
2021-12-08 01:26:44 +0300

    create urls for crud operations

--- student_management/student/urls.py
+++ student_management/student/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import index, students
+from .views import index, students, create_student, student_detail, update_student, delete_student
 
 urlpatterns = [
     path('', index, name='index'),
     path('students/', students, name='students'),
+    path('create/', create_student, name='create'),
+    path('students/<int:pk>/', student_detail, name='detail'),
+    path('edit/<int:pk>/', update_student, name='edit'),
+    path('delete/<int:pk>/', delete_student, name='delete'),
 ]
