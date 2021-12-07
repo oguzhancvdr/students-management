1db48173cb3c40f7bad02c59691754709104311f
ogzhncvdr <oguzhancavdar06@gmail.com>
2021-12-08 01:25:54 +0300

    create views for create update and delete student

--- student_management/student/views.py
+++ student_management/student/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Students
+from .forms import StudentForm
+
 
 def index(request):
     return render(request, 'index.html')
@@ -15,16 +18,53 @@ def students(request):
 
 
 def create_student(request):
-    pass
+    form = StudentForm()
+    if request.method == 'POST':
+        form = StudentForm(request.POST, request.FILES)
+        print(form.is_valid())
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Student has been created')
+            return redirect('students')
+    else:
+        form = StudentForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'form.html', context=context)
 
 
-def student_detail(request):
-    pass
+def student_detail(request, pk):
+    student = Students.objects.get(id=pk)
+
+    context = {
+        "student": student,
+    }
+
+    return render(request, 'student_detail.html', context=context)
 
 
-def update_student(request):
-    pass
+def update_student(request, pk):
+    student = Students.objects.get(id=pk)
+    form = StudentForm(instance=student)
+
+    if request.method == 'POST':
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+
+            return redirect('students')
+    context = {
+        "student": student,
+        'form': form,
+    }
+    return render(request, 'form.html', context=context)
 
 
-def delete_student(request):
-    pass
+def delete_student(request, pk):
+    student = Students.objects.get(id=pk)
+    student.delete()
+
+    return redirect('students')
